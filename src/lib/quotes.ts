0d3d23705735bd7cdef15b8e8090b9584da72db6
unknown <mairaijaz698@gmail.com>
2025-07-08 23:30:24 +0500

    Update: Added more quotes to each category in quotesData

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -3,35 +3,47 @@ export const quotesData: Record<string, { quote: string; author: string }[]> = {
     { quote: "Success is not final; failure is not fatal.", author: "Winston Churchill" },
     { quote: "Push yourself; no one else will.", author: "Unknown" },
     { quote: "Hard work beats talent when talent doesn't work hard.", author: "Tim Notke" },
-    { quote: "The secret of success is to do the common thing uncommonly well.", author: "John D. Rockefeller" }
+    { quote: "The secret of success is to do the common thing uncommonly well.", author: "John D. Rockefeller" },
+    { quote: "Success usually comes to those who are too busy to be looking for it.", author: "Henry David Thoreau" },
+    { quote: "Opportunities don't happen. You create them.", author: "Chris Grosser" }
   ],
   failure: [
     { quote: "Failure is the key to success.", author: "Morihei Ueshiba" },
     { quote: "Mistakes are proof you're trying.", author: "Jennifer Lim" },
     { quote: "Every failure is a step toward success.", author: "William Whewell" },
-    { quote: "Don't be afraid to fail. Be afraid not to try.", author: "Michael Jordan" }
+    { quote: "Don't be afraid to fail. Be afraid not to try.", author: "Michael Jordan" },
+    { quote: "Only those who dare to fail greatly can ever achieve greatly.", author: "Robert F. Kennedy" },
+    { quote: "Failure is simply the opportunity to begin again, this time more intelligently.", author: "Henry Ford" }
   ],
   learning: [
     { quote: "Learning never exhausts the mind.", author: "Leonardo da Vinci" },
     { quote: "The more you learn, the more you grow.", author: "Unknown" },
     { quote: "Education is the most powerful weapon.", author: "Nelson Mandela" },
-    { quote: "The beautiful thing about learning is that no one can take it away from you.", author: "B.B. King" }
+    { quote: "The beautiful thing about learning is that no one can take it away from you.", author: "B.B. King" },
+    { quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.", author: "Benjamin Franklin" },
+    { quote: "Develop a passion for learning. If you do, you will never cease to grow.", author: "Anthony J. D'Angelo" }
   ],
   inspiration: [
     { quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
     { quote: "Act as if what you do makes a difference. It does.", author: "William James" },
-    { quote: "With the new day comes new strength and new thoughts.", author: "Eleanor Roosevelt" }
+    { quote: "With the new day comes new strength and new thoughts.", author: "Eleanor Roosevelt" },
+    { quote: "What lies behind us and what lies before us are tiny matters compared to what lies within us.", author: "Ralph Waldo Emerson" },
+    { quote: "It always seems impossible until it's done.", author: "Nelson Mandela" }
   ],
   motivation: [
     { quote: "Don’t watch the clock; do what it does. Keep going.", author: "Sam Levenson" },
     { quote: "Start where you are. Use what you have. Do what you can.", author: "Arthur Ashe" },
     { quote: "Quality is not an act, it is a habit.", author: "Aristotle" },
-    { quote: "You are never too old to set another goal or to dream a new dream.", author: "C.S. Lewis" }
+    { quote: "You are never too old to set another goal or to dream a new dream.", author: "C.S. Lewis" },
+    { quote: "The way to get started is to quit talking and begin doing.", author: "Walt Disney" },
+    { quote: "The future depends on what you do today.", author: "Mahatma Gandhi" }
   ],
   life: [
     { quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
     { quote: "In the end, we only regret the chances we didn't take.", author: "Lewis Carroll" },
     { quote: "Life is short, and it is here to be lived.", author: "Kate Winslet" },
-    { quote: "Life is really simple, but we insist on making it complicated.", author: "Confucius" }
+    { quote: "Life is really simple, but we insist on making it complicated.", author: "Confucius" },
+    { quote: "To live is the rarest thing in the world. Most people exist, that is all.", author: "Oscar Wilde" },
+    { quote: "Life isn’t about finding yourself. It’s about creating yourself.", author: "George Bernard Shaw" }
   ]
 }
